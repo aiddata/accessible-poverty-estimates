86002eb88036b600d5e929508e643e5522a6a013
sgoodm <sgoodman@aiddata.wm.edu>
2022-06-22 12:06:04 -0400

    Update download_osm to use Prefect/Dask

--- src/download_osm.py
+++ src/download_osm.py
@@ -23,6 +23,11 @@ import hashlib
 import zipfile
 from pathlib import Path
 
+import prefect
+from prefect import task, Flow, Client
+from prefect.executors import DaskExecutor, LocalExecutor
+from prefect.run_configs import LocalRun
+
 
 if 'config.ini' not in os.listdir():
     raise FileNotFoundError("config.ini file not found. Make sure you run this from the root directory of the repo.")
@@ -30,33 +35,16 @@ if 'config.ini' not in os.listdir():
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-
 project = config["main"]["project"]
 project_dir = config["main"]["project_dir"]
-
 data_dir = Path(project_dir) / 'data'
 
-output_dir = data_dir / 'osm'
+dask_enabled = config["main"]["dask_enabled"]
+prefect_cloud_enabled = config["main"]["prefect_cloud_enabled"]
 
-osm_date = '220101'
 
-# key is region, value is list of country names in region
-country_dict = {
-    'africa': ['Liberia', 'Sierra Leone', 'Cameroon', 'Guinea', 'Mali', 'Nigeria', 'Zambia', 'Benin', 'Burundi', 'Ethiopia', 'South Africa', 'Uganda', 'Angola', 'Malawi', 'Rwanda', 'Tanzania', 'Zimbabwe', 'Chad', 'Ghana', 'Kenya', 'Lesotho'],
-    'antarctica': [],
-    'asia': [],
-    'australia': [],
-    'central-america': [],
-    'europe': [],
-    'north-america': [],
-    'south-america': []
-}
+# ---------------------------------------------------------
 
-download_list = []
-for region, country_names in country_dict.items():
-    download_list.extend([(region, i.lower().replace(' ', '-')) for i in country_names])
-
-base_url = 'http://download.geofabrik.de'
 
 def download_file(url, path, overwrite=False):
     if not path.exists() or overwrite:
@@ -68,6 +56,14 @@ def download_file(url, path, overwrite=False):
     return path
 
 
+@task
+def get_md5_contents(url, path):
+    md5_path = download_file(url, path)
+    md5 = open(md5_path,'rb').read().decode("utf-8").split(' ')[0]
+    return md5
+
+
+@task
 def download_and_verify(dl_url, dl_path, true_md5):
     valid = False
     overwrite = False
@@ -78,67 +74,104 @@ def download_and_verify(dl_url, dl_path, true_md5):
         overwrite = True
         dl_md5 = hashlib.md5(open(dl_path,'rb').read()).hexdigest()
         valid = dl_md5 == true_md5
-    return valid
 
-
-for region, country_name in download_list:
-
-    fname = f'{country_name}-{osm_date}-free.shp'
-    print(fname)
-
-    shp_path = output_dir / f'{fname}.zip'
-    shp_url = f'{base_url}/{region}/{fname}.zip'
-
-    md5_url = f'{base_url}/{region}/{fname}.zip.md5'
-    md5_path = output_dir / f'{fname}.zip.md5'
-
-    print('\tDownloading and/or verifying...')
-
-    _ = download_file(md5_url, md5_path)
-    true_md5 = open(md5_path,'rb').read().decode("utf-8").split(' ')[0]
-
-    valid = download_and_verify(shp_url, shp_path, true_md5)
     if not valid:
-        print(f'{fname} - failed to download and verify')
+        raise Exception('Failed to download and verify:', dl_url)
 
-    print('\tUnzipping...')
-
-    unzip_dir = output_dir / fname
-    if not unzip_dir.exists():
-        with zipfile.ZipFile(shp_path, 'r') as zip:
-            zip.extractall(unzip_dir)
+    return dl_path
 
 
-    osm_buildings_sqlite_path = data_dir / 'osm' / f'{country_name}-{osm_date}-free.shp' /'gis_osm_buildings_a_free_1.sqlite'
+@task
+def unzip(zip_path, out_dir):
+    if not out_dir.exists():
+        with zipfile.ZipFile(zip_path, 'r') as zip:
+            zip.extractall(out_dir)
 
-    if not osm_buildings_sqlite_path.exists():
+    return out_dir
 
-        osm_buildings_shp_path = data_dir / 'osm' / f'{country_name}-{osm_date}-free.shp' /'gis_osm_buildings_a_free_1.shp'
 
-        building_table_name = 'DATA_TABLE'
+@task
+def shapefile_to_spatialite(fname, dir, table_name):
+    shp_path = dir / f'{fname}.shp'
+    sqlite_path = dir / f'{fname}.sqlite'
+    if not sqlite_path.exists():
+        buildings_call_str = f'ogr2ogr -f SQLite -nlt PROMOTE_TO_MULTI -nln {table_name} -dsco SPATIALITE=YES {sqlite_path} {shp_path}'
 
-        buildings_call_str = f'ogr2ogr -f SQLite -nlt PROMOTE_TO_MULTI -nln {building_table_name} -dsco SPATIALITE=YES {osm_buildings_sqlite_path} {osm_buildings_shp_path}'
-
-        print('\tConverting buildings to spatialite...')
         buildings_call = sp.run(buildings_call_str, shell=True, capture_output=True)
 
         if buildings_call.returncode != 0:
             raise Exception(buildings_call.stderr, buildings_call_str)
 
 
-    osm_roads_sqlite_path = data_dir / 'osm' / f'{country_name}-{osm_date}-free.shp' / 'gis_osm_roads_free_1.sqlite'
+# ---------------------------------------------------------
 
-    if not osm_roads_sqlite_path.exists():
 
-        osm_roads_shp_path = data_dir / 'osm' / f'{country_name}-{osm_date}-free.shp' / 'gis_osm_roads_free_1.shp'
+osm_date = '220101'
 
-        road_table_name = 'DATA_TABLE'
+base_url = 'http://download.geofabrik.de'
 
-        roads_call_str = f'ogr2ogr -f SQLite -nlt PROMOTE_TO_MULTI -nln {road_table_name} -dsco SPATIALITE=YES {osm_roads_sqlite_path} {osm_roads_shp_path}'
+# key is region, value is list of country names in region
+country_dict = {
+    'africa': ['Liberia', 'Sierra Leone', 'Cameroon', 'Guinea', 'Mali', 'Nigeria', 'Zambia', 'Benin', 'Burundi', 'Ethiopia', 'South Africa', 'Uganda', 'Angola', 'Malawi', 'Rwanda', 'Tanzania', 'Zimbabwe', 'Chad', 'Ghana', 'Kenya', 'Lesotho'],
+    'antarctica': [],
+    'asia': ['Jordan', 'Pakistan', 'Philippines'],
+    'australia': [],
+    'central-america': [],
+    'europe': [],
+    'north-america': [],
+    'south-america': []
+}
 
-        print('\tConverting roads to spatialite...')
-        roads_call = sp.run(roads_call_str, shell=True, capture_output=True)
+download_list = []
+for region, country_names in country_dict.items():
+    download_list.extend([(region, i.lower().replace(' ', '-'), base_url, data_dir) for i in country_names])
 
-        if roads_call.returncode != 0:
-            raise Exception(roads_call.stderr, roads_call_str)
 
+with Flow("download-osm") as flow:
+    for input in download_list:
+        region, country_name, base_url, data_dir = input
+
+        fname = f'{country_name}-{osm_date}-free.shp'
+
+        output_dir = data_dir / 'osm'
+
+        shp_url = f'{base_url}/{region}/{fname}.zip'
+        shp_path = output_dir / f'{fname}.zip'
+
+        md5_url = f'{base_url}/{region}/{fname}.zip.md5'
+        md5_path = output_dir / f'{fname}.zip.md5'
+
+        unzip_dir = output_dir / fname
+
+
+        true_md5 = get_md5_contents(md5_url, md5_path)
+
+        shp_path = download_and_verify(shp_url, shp_path, true_md5)
+
+        unzip_dir = unzip(shp_path, unzip_dir)
+
+        sqlite_table_name = 'DATA_TABLE'
+
+        osm_buildings_sqlite_fname = 'gis_osm_buildings_a_free_1'
+        shapefile_to_spatialite(osm_buildings_sqlite_fname, unzip_dir, sqlite_table_name)
+
+        osm_roads_sqlite_fname = 'gis_osm_roads_free_1'
+        shapefile_to_spatialite(osm_roads_sqlite_fname, unzip_dir, sqlite_table_name)
+
+
+
+if dask_enabled:
+    executor = DaskExecutor(address="tcp://127.0.0.1:8786")
+else:
+    executor = LocalExecutor()
+
+# flow.run_config = LocalRun()
+flow.executor = executor
+
+if prefect_cloud_enabled:
+    flow_id = flow.register(project_name="accessible-poverty-estimates")
+    client = Client()
+    run_id = client.create_flow_run(flow_id=flow_id)
+
+else:
+    state = flow.run()
